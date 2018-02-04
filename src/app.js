628e533f368783b2236f3c160a503e9bae3577c1
koushikui <koushikreddygunapati@gmail.com>
2018-02-03 23:31:55 -0500

    new boiler plate

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
-import IndecisionApp from './components/IndecisionApp';
-import Forms from './components/Forms';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>This is boiler plate </p>, document.getElementById('app'));
--- src/playground/es6-let-const.js
+++
@@ -1,2 +0,0 @@
-const getFirstName = (x) => x.split(' ')[0];
-console.log(getFirstName('Mike Smith'));
